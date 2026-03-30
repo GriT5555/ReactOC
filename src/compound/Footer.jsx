52aeaafd18a2ebc6a505323c87c2543b3fc1ab4b
Mat <113144372+GriT5555@users.noreply.github.com>
2026-03-30 13:20:21 +0200

    Footer pic fix
    Import and use

--- src/compound/Footer.jsx
+++ src/compound/Footer.jsx
@@ -1,9 +1,12 @@
+import logo2 from "../assets/Kasa2.webp"
+import footerimg from "../assets/kasafooter.webp"
+
 export default function Footer() {
     return(
         <footer className="footer">
             <div className="footerwebp">
-                <img className="footerimg" src={"./src/assets/Kasa2.webp"} alt="" />
-                <img className="footerimg" src={"./src/assets/kasafooter.webp"} alt="" />
+                <img className="footerimg" src={logo2} alt="" />
+                <img className="footerimg" src={footerimg} alt="" />
             </div>
         </footer>
     )
