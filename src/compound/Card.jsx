a48f98f5c84e2792a55f0f4970677406512b883c
Mat <113144372+GriT5555@users.noreply.github.com>
2026-03-30 11:31:41 +0200

    Card fix
    declaring correctly "item" term before using it

--- src/compound/Card.jsx
+++ src/compound/Card.jsx
@@ -7,13 +7,11 @@ export default function Card() {
         <div className="CardsContainer">
             {Data.map((item) => (
                 <Link className="Cards" to={`/logement/${item.id}`} key={item.id}>
-
-                        <img className="CardsPics" src={item.cover} alt={item.title}/>
-                        <h2 className="CardTitle">{item.title}</h2>
-
+                    <img className="CardsPics" src={item.cover} alt={item.title}/>
+                    <h2 className="CardTitle">{item.title}</h2>
                 </Link>
             ))}
         </div>
    
     );
-}
+}   
